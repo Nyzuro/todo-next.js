13000df9fd2d40cae8262bc752436c5666f41959
Nyzuro <marco.strs27@gmail.com>
2025-08-01 10:56:05 +0200

    ✨ Display the todo list on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import { buttonVariants } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { prisma } from "@/lib/prisma";
 import Link from "next/link";
 
 export default async function Page() {
+	const todoList = await prisma.todo.findMany({
+		orderBy: {
+			createdAt: "desc",
+		},
+	});
+
 	return (
 		<Card>
 			<CardHeader>
@@ -16,6 +23,17 @@ export default async function Page() {
 				>
 					Create a to-do list
 				</Link>
+
+				{todoList.map((todo) => (
+					<Card className="p-4" key={todo.id}>
+						<CardHeader>
+							<CardTitle>
+								<p>{todo.title}</p>
+							</CardTitle>
+						</CardHeader>
+						<p>{todo.task}</p>
+					</Card>
+				))}
 			</CardContent>
 		</Card>
 	);
